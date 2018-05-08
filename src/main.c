086c11527fcd62a98f09403409e33bee59cde223
Stanisław J <>
2018-05-08 08:26:03 +0200

    test

--- src/main.c
+++ src/main.c
@@ -11,7 +11,9 @@
 
 #include "stm32f4xx.h"
 #include "stm32f4_discovery.h"
-
+#include "diskio.h"
+#include "tm_stm32f4_fatfs.h"
+#include "tm_stm32f4_delay.h"
 
 int main(void)
 {
@@ -73,6 +75,56 @@ int main(void)
 	        USART_SendData(USART1, messageText[i]);
 	        i++;
 	    }
+
+
+
+
+	    //SD CARD
+
+	    	//Fatfs object
+	        FATFS FatFs;
+	        //File object
+	        FIL fil;
+	        //Free and total space
+	        uint32_t total, free;
+
+	        //Initialize system
+	        SystemInit();
+	        //Initialize delays
+	        TM_DELAY_Init();
+	        //Initialize LEDs
+	        TM_DISCO_LedInit();
+
+	        //Mount drive
+	        if (f_mount(&FatFs, "", 1) == FR_OK) {
+	            //Mounted OK, turn on RED LED
+
+
+	            //Try to open file
+	            if (f_open(&fil, "1stfile.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE) == FR_OK) {
+	                //File opened, turn off RED and turn on GREEN led
+
+
+	                //If we put more than 0 characters (everything OK)
+	                if (f_puts("First string in my file\n", &fil) > 0) {
+	                    if (TM_FATFS_DriveSize(&total, &free) == FR_OK) {
+	                        //Data for drive size are valid
+	                    }
+
+	                    //Turn on both leds
+
+	                }
+
+	                //Close file, don't forget this!
+	                f_close(&fil);
+	            }
+
+	            //Unmount drive, don't forget this!
+	            f_mount(0, "", 1);
+
+
+
+
 
 	    // Infinite loop
 	    while (1){}
