340c1365431e2ec25f920c7fdd80356ddeb986da
Stanisław J <>
2018-05-29 16:35:53 +0200

    Przyciski

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@
 
 
 
+
 #include "stm32f4xx.h"
 #include <stdio.h>
 #include <string.h>
@@ -21,7 +22,6 @@ int main(void)
 {
 RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 
 // tworzenie strutkury
 GPIO_InitTypeDef  GPIO_InitStructure;
@@ -34,12 +34,13 @@ GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
 GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 GPIO_Init(GPIOD, &GPIO_InitStructure);
 
-GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
-GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-GPIO_Init(GPIOA, &GPIO_InitStructure);
+GPIO_InitTypeDef Przyciski;
+	Przyciski.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_2;
+	Przyciski.GPIO_Mode = GPIO_Mode_IN;
+	Przyciski.GPIO_OType = GPIO_OType_PP;
+	Przyciski.GPIO_Speed = GPIO_Speed_100MHz;
+	Przyciski.GPIO_PuPd = GPIO_PuPd_UP;
+GPIO_Init(GPIOA, &Przyciski);
 
 
 
@@ -56,19 +57,19 @@ TM_HD44780_Init(24,4);
 TM_HD44780_Clear();
 TM_HD44780_Puts(0, 0, &dane);
 
+do{
+		if(!GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_1))
+		{
+			TM_HD44780_Clear();
+			TM_HD44780_Puts(0, 0, "1");
+		}
+		if(!GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_2))
+		{
+			TM_HD44780_Clear();
+			TM_HD44780_Puts(0, 0, "2");
+		}
+		Delayms(150);
 
-for(;;)
-{
-	if(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0)==1)
-	{GPIO_SetBits(GPIOD,GPIO_Pin_12);
-	TM_HD44780_Clear();
-	TM_HD44780_Puts(0, 0, "WITAJ");
-
-	}
-
-
-}
-
-
+	}while(1);
 
 }
